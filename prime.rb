664ddeab5446454e96f475a8d5a6b813d7f726aa
Stephanie Messad <sabud1208@gmail.com>
2018-11-21 02:18:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,9 +6,9 @@ def prime?(num)
 end
   (1...num).to_a.each do |number|
     if num % number == 0
-      prime=false
+      prime = false
     else
-      prime=true
+      prime = true
   end
 end
 end
