c947f4fbc62ef543bf8b13f08f21f6d7179ca247
Stephanie Messad <sabud1208@gmail.com>
2018-11-21 02:21:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@ require "pry"
 
 def prime?(num)
   if num < 2
-  prime = false
+    prime = false
 end
   (1...num).to_a.each do |number|
-    if num % number == 0
+  if num % number == 0
     prime = false
   end
 end
