5b38e1c25b0328bf154fac4844675b854b3585d9
Stephanie Messad <sabud1208@gmail.com>
2018-11-21 02:24:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+require "pry"
+
+def prime?(num)
+  if num < 2
+     return false
+end
+  (2...num).to_a.each do |number|
+  if num % number == 0
+      return false
+  end
+end
+    return true
+end
