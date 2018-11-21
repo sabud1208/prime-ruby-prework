8cf6fed4593c7b8452016aab73130d7663d94654
Stephanie Messad <sabud1208@gmail.com>
2018-11-21 02:06:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require "pry"
+
+def prime?(num)
+counter = 2
+counter == 9
+  if num % counter == 0 || num < 2 || num % counter == 1
+     prime= true
+        else
+     prime= true
+  end
+  counter += 1
+end
+  prime
+end
