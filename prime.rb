736466a1f843089390cded53c5bc12bd06b5d807
Stephanie Messad <sabud1208@gmail.com>
2018-11-21 02:07:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,12 @@
 require "pry"
 
 def prime?(num)
-counter = 2
-counter
+
   if num % counter == 0 || num < 2 || num % counter == 1
      prime= true
         else
      prime= true
   end
-  
 end
   prime
 end
